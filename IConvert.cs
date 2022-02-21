b78aa287e8a444f5c9fd4438f0afea3896ee9693
cat32tw <mike1993xd@gmail.com>
2022-02-21 21:18:36 +0800

    介面範例

---
+++ IConvert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface1
+{
+    internal interface IConvert
+    {
+        double Mult { get; set; }       // 倍率屬性
+        void Convert(double value);     // 換算長度的方法原型
+    }
+}
